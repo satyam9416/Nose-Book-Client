2e953e6d7d43cd7ecb0f887812fb560445044ff5
Satyam9416 <satyam94163@gmail.com>
2023-03-07 07:55:21 +0530

    optimized for better performance

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
-import Signup from './components/auth/signup/signup'
-import Login from './components/auth/login/login'
-import Home from './pages/home';
 import PrivateRoute from './components/custom-routes/privateRoute';
-import Profile from './pages/profile';
-import Chat from './pages/Chat';
+import './app.css'
+const Home = lazy(() => import('./pages/home'));
+const Profile = lazy(() => import('./pages/profile'));
+const Chat = lazy(() => import('./pages/Chat'));
+const Signup = lazy(() => import('./components/auth/signup/signup'));
+const Login = lazy(() => import('./components/auth/login/login'));
 
 const App = () => {
 
@@ -13,13 +14,23 @@ const App = () => {
     <Routes>
       <Route path='*' element={<h1>page not found</h1>} />
       <Route path='/' element={<PrivateRoute />} >
-        <Route path='/' element={<Navigate to='home' />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/profile/:id' element={<Profile />} />
-        <Route path='/chat/:id' element={<Chat />} />
+        <Route index element={<Navigate to='home' />} />
+        <Route path='/home' element={
+          <Suspense fallback={<h1>Loading...</h1>}><Home /></Suspense>
+        } />
+        <Route path='/profile/:id' element={
+          <Suspense fallback={<h1>Loading...</h1>}><Profile /></Suspense>          
+        } />
+        <Route path='/chat/:id' element={
+          <Suspense fallback={<h1>Loading...</h1>}><Chat /></Suspense>    
+        } />
       </Route>
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={
+        <Suspense fallback={<h1>Loading...</h1>}><Signup /></Suspense>    
+      } />
+      <Route path='/login' element={
+        <Suspense fallback={<h1>Loading...</h1>}><Login /></Suspense>    
+      } />
     </Routes>
   )
 }
